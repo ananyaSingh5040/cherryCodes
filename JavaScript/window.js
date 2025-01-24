5fa983a70f2ca25e26836954f7224870d3074734
Ananya Singh <singhananya5040@gmail.com>
2025-01-24 11:13:26 +0530

    JS

---
+++ JavaScript/window.js
@@ -0,0 +1,10 @@
+//Window objects, DOM and BOM:
+
+window.console.log(window);
+//a global object is window.
+//window has dom , bom and core javascript.
+//dom is a javacript object which is your html only.
+//bom are additional objects provided by the host environment like alert, confirm, prompt , location.href etc.
+
+console.log(document);
+// document.body.style.background="yellow"; use it in browser.

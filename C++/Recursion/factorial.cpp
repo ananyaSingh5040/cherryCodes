0a96618edd0ca12ef287f4d4dcabba2ac6970ca3
Ananya Singh <singhananya5040@gmail.com>
2025-02-19 10:53:41 +0530

    19-02-25

--- C++/Recursion/factorial.cpp
+++ C++/Recursion/factorial.cpp
@@ -19,3 +19,34 @@ int main()
 
     return 0;
 }
+// #include <iostream>
+// using namespace std;
+// int factorial(int n,int key)
+// {
+    
+//     if (n <= 1){
+//     cout<<1<<" ";
+//         return 1;
+//     }
+//     else{
+//         int fact= n * factorial(n - 1,key);
+//         if(fact<=key)
+//         {
+//             cout<<"fact: "<<fact<<endl;
+//         }
+//         return fact;
+        
+//     }
+    
+    
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n: ";
+//     cin >> n;
+//     cout << factorial(n,6);
+
+//     return 0;
+// }

ab3398d429d76ec9c6cd1615e5316a599487485a
Ananya Singh <singhananya5040@gmail.com>
2025-02-19 10:53:27 +0530

    19-02-25

---
+++ C++/Recursion/checkSubsequences.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//void returnSubsequences (vector arr, vector ans,int i,int n)
+// {
+//     if(i==n)
+//     {
+//        //print ans;
+//        return;
+//     }
+//     else{
+
+//         //include
+
+//         //ans.push_back(arr[i]);
+//         returnSubsequences(arr,ans,i,n);
+//         //exclude
+
+//         //ans.pop_back()
+//         returnSubsequences(arr,ans,i,n);
+//     }
+// }
+int main()
+{
+    return 0;
+}

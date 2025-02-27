5c941be160a45af54d858e4e5d4c183a6e9b91c0
Ananya Singh <singhananya5040@gmail.com>
2025-02-27 15:44:23 +0530

    27-02-25

--- DSA/Linked Lists/kplaces.cpp
+++ DSA/Linked Lists/kplaces.cpp
@@ -1,32 +1,32 @@
-ListNode* getNode(ListNode* temp, int n)
-{
-    int cnt =1;
-    while(temp!=NULL)
-    {
-        if(cnt ==n) return temp;
-        cnt++;
-        temp = temp->next;
-    }
-    return temp;
-}
-    ListNode* rotateRight(ListNode* head, int k) {
+// ListNode* getNode(ListNode* temp, int n)
+// {
+//     int cnt =1;
+//     while(temp!=NULL)
+//     {
+//         if(cnt ==n) return temp;
+//         cnt++;
+//         temp = temp->next;
+//     }
+//     return temp;
+// }
+//     ListNode* rotateRight(ListNode* head, int k) {
 
-        if(head==NULL|| k==0) return head;
-        ListNode* temp = head;
-        int n=1;
-        while(temp->next!=NULL)
-        {
-            temp = temp->next;
-            n++;
-        }
-         k = k%n;
-        if(k==0) return head;
+//         if(head==NULL|| k==0) return head;
+//         ListNode* temp = head;
+//         int n=1;
+//         while(temp->next!=NULL)
+//         {
+//             temp = temp->next;
+//             n++;
+//         }
+//          k = k%n;
+//         if(k==0) return head;
 
-        temp->next = head;
-        ListNode * newLastNode = getNode(head, n-k);
-        head = newLastNode->next;
-        newLastNode->next = NULL;
-        return head;
+//         temp->next = head;
+//         ListNode * newLastNode = getNode(head, n-k);
+//         head = newLastNode->next;
+//         newLastNode->next = NULL;
+//         return head;
         
         
-    }
+//     }

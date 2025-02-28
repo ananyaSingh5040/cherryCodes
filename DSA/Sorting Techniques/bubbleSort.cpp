a2bfdc59c9f08e99088e7a053a8f4b5fdc3f918f
Ananya Singh <singhananya5040@gmail.com>
2025-02-28 10:17:53 +0530

    28-02-25

---
+++ DSA/Sorting Techniques/bubbleSort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n = 3;
+    int arr[3] = {18, 16, 7};
+    // BUBBLE SORT:
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    // Printing the array:
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

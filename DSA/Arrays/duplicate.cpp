18373181cf5f02e5564de1c573e92bf16fb67020
Ananya Singh <singhananya5040@gmail.com>
2025-03-08 20:39:07 +0530

    08-03-25

---
+++ DSA/Arrays/duplicate.cpp
@@ -0,0 +1,14 @@
+// int removeDuplicates(vector<int>& nums) {
+//     int k= nums.size();
+//    int i=0;
+//    for(int j=1; j<k;j++)
+//    {
+//        if(nums[j]!=nums[i])
+//        {
+//            nums[i+1] = nums[j];
+//            i++;
+//        }
+//    }
+//    return i+1;
+   
+// }
